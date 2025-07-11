13a5cd78304fca90805ad6ff68618a9ecf8172ac
Himanshu <himanshu1sharma88@gmail.com>
2025-07-11 23:22:33 +0530

    updated controller

--- src/main/java/com/chat/app/controller/ChatController.java
+++ src/main/java/com/chat/app/controller/ChatController.java
@@ -1,22 +1,28 @@
 package com.chat.app.controller;
 
-import com.chat.app.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.chat.app.model.ChatMessage;
 
 @Controller
 public class ChatController {
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
-    public ChatMessage sendMessage(ChatMessage message){
+    public ChatMessage sendMessage(ChatMessage message) {
         return message;
     }
 
-    @GetMapping("chat")
+    @GetMapping("/")
+    public String redirectToChat() {
+        return "redirect:/chat";
+    }
+
+    @GetMapping("/chat")
     public String chat() {
-        return "chat";
+        return "chat"; // chat.html in templates
     }
 }
